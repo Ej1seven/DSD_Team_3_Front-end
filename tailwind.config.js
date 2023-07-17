dbeda396aaa4bcf934cee3f1cc4e1d3149a5bc00
Ej1seven <erikhunter@erikhunter.dev>
2023-07-17 13:26:16 -0500

    added landing page

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,6 +13,28 @@ module.exports = {
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
     },
+    screens: {
+      xsm: '425px',
+      // => @media (min-width: 425px) { ... }
+      sm: '640px',
+      // => @media (min-width: 640px) { ... }
+
+      md: '768px',
+      // => @media (min-width: 768px) { ... }
+
+      lg: '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      xl: '1280px',
+      // => @media (min-width: 1280px) { ... }
+
+      '2xl': '1536px',
+      // => @media (min-width: 1536px) { ... }
+      exSMMAX: { min: '0px', max: '425px' },
+      // => @media (min-width: 640px and max-width: 767px) { ... }
+      smMAX: { min: '0px', max: '767px' },
+      // => @media (min-width: 640px and max-width: 767px) { ... }
+    },
   },
   plugins: [],
-}
+};
