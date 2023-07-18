89b0185547b1dcee764a8c7a89ad9d0419b36793
Ej1seven <erikhunter@erikhunter.dev>
2023-07-17 23:00:45 -0500

    updated landing page

--- app/page.tsx
+++ app/page.tsx
@@ -7,11 +7,6 @@ import LandingPage from './pages/landing_page/page';
 export default function Home() {
   return (
     <div className="relative bg-white w-full h-[982px] overflow-hidden text-center text-xl text-base-black font-poppins">
-      <img
-        className="absolute bottom-[-10rem] left-[-10rem] min-w-[675.84px] min-h-[735.89px]"
-        alt=""
-        src="/decore.svg"
-      />
       <Navbar />
       <LandingPage />
     </div>
