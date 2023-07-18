e2e03a4161a99e259d8ffbd4d8eff209d975e6ee
Ej1seven <erikhunter@erikhunter.dev>
2023-07-17 20:51:41 -0500

    pulling updates

---
+++ app/member_homepage/page.tsx
@@ -0,0 +1,14 @@
+import Image from 'next/image';
+
+import { FunctionComponent } from 'react';
+
+const MemberHomepage: FunctionComponent = () => {
+  return (
+    <div className="flex w-[1100px] h-full mx-auto border-2 border-black">
+      <div className="flex-1"></div>
+      <div></div>
+    </div>
+  );
+};
+
+export default MemberHomepage;
--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@
 
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import LandingPage from './landing_page/page';
+import MemberHomepage from './member_homepage/page';
 
 export default function Home() {
   return (
