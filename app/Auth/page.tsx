53c5cffa99b61f1030859a1aabb60900dabc2234
Daisy <daisy.le0203@gmail.com>
2023-07-17 19:56:02 -0500

    add axios and create a helper function for it

--- app/Auth/page.tsx
+++ app/Auth/page.tsx
@@ -1,6 +1,7 @@
 "use client"
 
-import { FunctionComponent, useState } from "react"
+import { FunctionComponent, useState, useEffect } from "react"
+import { request, setAuthHeader } from "../helpers/axios_helper"
 
 const AuthPage: FunctionComponent = () => {
   const [currentScreen, setCurrentScreen] = useState<"login" | "register">(
@@ -8,16 +9,38 @@ const AuthPage: FunctionComponent = () => {
   )
   const [email, setEmail] = useState<string>("")
   const [password, setPassword] = useState<string>("")
+  const [data, setData] = useState([])
+
+  useEffect(() => {
+    request("GET", "/messages", {})
+      .then((response) => {
+        setData(response.data)
+      })
+      .catch((error) => {
+        if (error.response?.status === 401) {
+          setAuthHeader(null)
+        } else {
+          setData([error.response?.code])
+        }
+      })
+  }, [])
 
   const handleLogin = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault()
-    // You can add your login logic here
+    // add login logic here
     console.log("Email:", email)
     console.log("Password:", password)
   }
 
   return (
     <div className="min-h-screen flex items-center justify-center bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
+      {/* <ul>
+        {data.map((line, index) => (
+          <li key={index} className="border-b border-gray-200 py-2">
+            {line}
+          </li>
+        ))}
+      </ul> */}
       <div className="max-w-md w-full space-y-8">
         {currentScreen === "login" ? (
           <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
