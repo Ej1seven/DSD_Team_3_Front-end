6c04b8814dde6c1686862ad13902177094f3da88
Ej1seven <erikhunter@erikhunter.dev>
2023-07-21 11:58:05 -0500

    updated the landing page

---
+++ app/components/NavbarMobileButton.tsx
@@ -0,0 +1,8 @@
+const NavbarMobileButton = () => {
+  return (
+    <div className="absolute top-[12px] right-[12px] w-12 h-12 object-cover lg:hidden">
+      <img className="" alt="" src="/square.png" />
+    </div>
+  );
+};
+export default NavbarMobileButton;
